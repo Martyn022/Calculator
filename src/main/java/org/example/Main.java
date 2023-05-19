991ad67b0ce5ae6cccf036c20d777532f6eb35d9
Martyn022 <mgsharipov@gmail.com>
2023-05-19 11:46:51 +0500

    second commit, добавил классы Ints и IntsCalculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(22, 33));
+        System.out.println(intsCalc.sub(21, 2));
+        System.out.println(intsCalc.div(10, 5));
+        System.out.println(intsCalc.mult(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
     }
+
 }
